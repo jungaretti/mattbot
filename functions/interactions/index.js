45129f34d9da7d86179db4a3d36ef48bad4e76cd
JP Ungaretti <hello@jungaretti.com>
2021-07-07 19:31:08 -0500

    Implement basic bot

--- functions/interactions/index.js
+++ functions/interactions/index.js
@@ -1,13 +1,75 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const nacl = require("tweetnacl");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+function verifyRequest(req) {
+  // prettier-ignore
+  const publicKey="71c0aebfcef970766575bc42497d1b9334b97b62fd6879a88d4a815f8536f864";
+  const signature = req.headers["x-signature-ed25519"];
+  const timestamp = req.headers["x-signature-timestamp"];
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  return (
+    signature &&
+    timestamp &&
+    nacl.sign.detached.verify(
+      Buffer.from(timestamp + req.rawBody),
+      Buffer.from(signature, "hex"),
+      Buffer.from(publicKey, "hex")
+    )
+  );
 }
+
+const resInv = () => {
+  return {
+    status: 401,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      error: "Invalid request signature",
+    },
+  };
+};
+
+const resAck = () => {
+  return {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      type: 1,
+    },
+  };
+};
+
+const resMsg = (content) => {
+  return {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      type: 4,
+      data: {
+        content,
+      },
+    },
+  };
+};
+
+module.exports = async function (context, req) {
+  if (!verifyRequest(req)) {
+    // Ignore unsigned requests
+    context.res = resInv();
+    return;
+  }
+
+  const interaction = req.body;
+  if (interaction.type === 1) {
+    // Acknowledge pings
+    context.res = resAck();
+  } else {
+    switch (interaction.command) {
+      default:
+        context.res = resMsg("Alright, alright, alright!");
+        break;
+    }
+  }
+};
